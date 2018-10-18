ca620fbe21ba2e09b350d2c223488f63d21d7635
ogigg <gierszon10@gmail.com>
2018-10-18 17:55:17 +0200

    Added filtering to plant list

--- Growth/ClientApp/src/app/components/plant-list/plant-list.component.ts
+++ Growth/ClientApp/src/app/components/plant-list/plant-list.component.ts
@@ -1,3 +1,4 @@
+import { OrderService } from './../../services/order.service';
 import { Plant } from './../plant.model';
 import { Component, OnInit } from '@angular/core';
 import { PlantService } from './../../services/plant.service';
@@ -11,18 +12,34 @@ import { SpeciesService } from './../../services/species.service';
   styleUrls: ['./plant-list.component.css']
 })
 export class PlantListComponent implements OnInit {
-  plants: Plant[];
+  displayedPlants: Plant[];
+  allPlants
+  orders: any[];
+  filter: any = {};
 
-  constructor(private plantService: PlantService,private speciesService: SpeciesService) {
+  constructor(private plantService: PlantService,private speciesService: SpeciesService, private orderService: OrderService) {
     
    }
 
   ngOnInit() {
+    this.orderService.getOrders().subscribe((orders: any[]) => this.orders = orders);
     this.plantService.getPlants().subscribe((plants: Plant[]) => {
-      this.plants = plants;
-      console.log(this.plants);
+      this.displayedPlants = this.allPlants = plants;
+      console.log(this.displayedPlants);
     });
     
+  }
+  onFilterChange(){
+    var plants = this.allPlants;
+    if(this.filter.orderId)
+    {
+      plants=plants.filter(p=>p.orderId==this.filter.orderId);
+    }
+    this.displayedPlants=plants;
+  }
+  resetFilter(){
+    this.filter={};
+    this.onFilterChange();
   }
 
 }
